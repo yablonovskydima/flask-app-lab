91c47c411d431dd77ccc480b8f2f6c6d797453bf
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2025-10-13 00:22:22 +0300

    Added basic templates, style and routing

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return "Hello World"
+def aboutme():
+    return render_template('aboutme.html', title="About Me")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Контакти")
 
 if __name__ == '__main__':
     app.run(debug=True)
